98d27849d7e19a38777e83e02c9b14f5774e87ae
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-06-22 14:54:29 -0700

    added experience stack component

---
+++ src/components/ExperienceStack.tsx
@@ -0,0 +1,43 @@
+"use client";
+import ExperienceCard from "@/components/features/ExperienceCard";
+
+export default function ExperienceList() {
+  const experiences = [
+    {
+      logoSrc: "/filler.jpg",
+      companyName: "Company1",
+      title: "Role",
+      startDate: "September 2024",
+      description: "Worked on...",
+    },
+    {
+      logoSrc: "/filler.jpg",
+      companyName: "Company2",
+      title: "Role",
+      startDate: "September 2024",
+      description: "Worked on...",
+    },
+    {
+      logoSrc: "/filler.jpg",
+      companyName: "Company3",
+      title: "Role",
+      startDate: "August 2023",
+      description: "Worked on...",
+    },
+  ];
+
+  return (
+    <div className="max-w-[800px] mx-auto space-y-[15px] ">
+      {experiences.map((exp, idx) => (
+        <ExperienceCard
+          key={idx}
+          logoSrc={exp.logoSrc}
+          companyName={exp.companyName}
+          title={exp.title}
+          startDate={exp.startDate}
+          description={exp.description}
+        />
+      ))}
+    </div>
+  );
+}
