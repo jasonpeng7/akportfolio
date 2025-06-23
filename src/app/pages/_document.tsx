f00116027db5466dcc23120ff37e68263d8a0bef
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-06-23 01:18:27 -0700

    custom fonts

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
           crossOrigin="anonymous"
         />
 
-        {/* Your Google Fonts */}
+        {/* add more fonts here if supplied with link */}
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap"
           rel="stylesheet"
