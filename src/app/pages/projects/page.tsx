7945e5e079d627d9288a62197ad27dee2f89cfb3
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-06-23 23:43:50 -0700

    ui update

--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -5,23 +5,32 @@ const allProjects = [
   { name: "Project2", slug: "test-2", imageUrl: "/filler.jpg" },
   { name: "Project3", slug: "test-3", imageUrl: "/filler.jpg" },
   { name: "Project4", slug: "test-4", imageUrl: "/filler.jpg" },
+  // Add more projects here directly
 ];
 
 export default function ProjectsPage() {
   return (
-    <div className="">
-      <h1 className="w-fit poppins-semibold text-primary-grey uppercase tracking-regular mt-0 mb-1 border-b-1 pb-1 border-primary-grey">
-        All Projects
-      </h1>
-      <div className="grid grid-cols-1 sm:flex sm:flex-row gap-6">
-        {allProjects.map((project) => (
-          <ProjectCard
-            key={project.slug}
-            slug={project.slug}
-            name={project.name}
-            imageUrl={project.imageUrl}
-          />
-        ))}
+    <div className="space-y-10">
+      <div>
+        <h1 className="w-fit poppins-regular text-primary-navy tracking-regular mt-0 mb-1 border-b-1 pb-1 border-primary-grey">
+          All Projects
+        </h1>
+      </div>
+
+      <div className="">
+        <h2 className="text-3xl poppins-semibold text-primary-navy mb-6">
+          Dive into my experience
+        </h2>
+        <div className="flex flex-wrap justify-center gap-8 md:grid md:grid-cols-2 md:gap-x-6 md:gap-y-10 lg:grid-cols-3 xl:grid-cols-4">
+          {allProjects.map((project) => (
+            <ProjectCard
+              key={project.slug}
+              slug={project.slug}
+              name={project.name}
+              imageUrl={project.imageUrl}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
