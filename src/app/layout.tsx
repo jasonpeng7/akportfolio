36042af9b569f6b50fb296440f285489ea5d381d
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-06-23 22:16:27 -0700

    fixed padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <Navbar />
         <Sidenav />
         <main className="pt-20 md:ml-64 transition-all duration-300 ease-in-out">
-          <div className="p-2 sm:p-4 lg:p-6">{children}</div>
+          <div className="p-4">{children}</div>
         </main>
       </body>
     </html>
