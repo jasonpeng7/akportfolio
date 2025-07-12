26cc8db979fd616a3f94ee5362e984ed835bb82d
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-07-12 15:54:58 -0700

    created component for project data

---
+++ src/data/projects.ts
@@ -0,0 +1,58 @@
+export const allProjects = [
+  {
+    name: "(Personal) Hub Motor Project",
+    overview:
+      "Transferring interest from electro-mechanical engineering into real world applications.",
+    description: [
+      {
+        title: "Design",
+        text: "Designing the hub motor components using CAD software, focusing on manufacturability and performance.",
+      },
+      {
+        title: "Simulation",
+        text: "Performing FEA and thermal simulations to analyze structural integrity and heat dissipation under load.",
+      },
+      {
+        title: "Prototyping",
+        text: "Utilizing 3D printing and manual machining to create functional prototypes for real-world testing and validation.",
+      },
+    ],
+    slug: "hub-motor-project",
+    imageUrl: "/filler.jpg",
+  },
+  {
+    name: "(ARMS) NVD1500 DCG 3-axis CNC Mill Servicing",
+    overview:
+      "Restoring a DMG Mori CNC mill to full operational status for graduate/undergraduate research opportunities.",
+    description: [
+      {
+        title: "Vice Prototyping",
+        text: "Measuring the needs of a vice and testing the efficacy of a 3D-printed prototype.",
+      },
+      {
+        title: "Vice CAM and Machining",
+        text: "Machining the Vice on a CNC mill using irregular stock requiring advanced workholding.",
+      },
+      {
+        title: "Repairs & Maintenance",
+        text: "Coolant tank cleaning and refilling, as well as system maintenance.",
+      },
+    ],
+    slug: "arms-nvd1500-dcg-3-axis-cnc-mill-servicing",
+    imageUrl: "/filler.jpg",
+  },
+  {
+    name: "(ARMS) MicroVu Measurement Systems",
+    overview:
+      "Applying precision measurement techniques in aid of graduate level manufacturing research at ARMS lab.",
+    slug: "microvu-measurement-systems",
+    imageUrl: "/filler.jpg",
+  },
+  {
+    name: "(FSAE) Machined Parts",
+    overview:
+      "Applying manual Lathe and Bridgeport Mill manufacturing skills to create steel and aluminum vehicle parts.",
+    slug: "machined_parts",
+    imageUrl: "/filler.jpg",
+  },
+];
